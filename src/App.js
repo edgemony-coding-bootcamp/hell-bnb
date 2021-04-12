fa5fddce6ac0323bd793b64d387c9e1a1eaf904a
bruteforce91 <corsarosalvatore91@yahoo.it>
2021-04-12 17:39:39 +0200

    feat:routing logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header/Header.jsx";
+import Activities from "./Activities.jsx";
+import BannerWrap from "./Components/BannerCookies/BannerCookies.jsx";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <BannerWrap />
+          </Route>
+          <Route exact path="/activities/:id">
+            <Activities />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
