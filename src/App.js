7d641a819ea4da3ba51b302df3f177e58cbc0e3f
Michelangelo Morabito <michimorabito@gmail.com>
2021-04-14 18:14:02 +0200

    feat: implemented static Activity page, modified Rank component so it can also work with ActivityTitle

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header/Header.jsx";
-import Activities from "./Activities.jsx";
-import Homepage from "./components/Homepage/Homepage.jsx";
-import { useState, useCallback } from "react";
-import BannerCookies from "./components/BannerCookies/BannerCookies.jsx";
-import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './Components/Header/Header'
+import Activity from './pages/Activity'
+import Homepage from './Components/Homepage/Homepage'
+import { useState, useCallback } from 'react'
+import BannerCookies from './Components/BannerCookies/BannerCookies'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   const [isBannerVisible, setIsBannerVisible] = useState(
-    localStorage.getItem("cookieSession")
-  );
+    localStorage.getItem('cookieSession')
+  )
 
   const cookieSession = useCallback(() => {
-    const date = JSON.stringify(new Date());
-    localStorage.setItem("cookieSession", `date:${date}`);
-    setIsBannerVisible(true);
-    return;
-  }, []);
+    const date = JSON.stringify(new Date())
+    localStorage.setItem('cookieSession', `date:${date}`)
+    setIsBannerVisible(true)
+    return
+  }, [])
 
   return (
     <>
       <Router>
-        <div className="App">
+        <div className='App'>
           <Header />
           <Switch>
-            <Route exact path="/">
+            <Route exact path='/'>
               <Homepage />
             </Route>
-            <Route exact path="/activities/:id">
-              <Activities />
+            <Route exact path='/activities/:id'>
+              <Activity />
             </Route>
           </Switch>
           <BannerCookies
@@ -39,7 +39,7 @@ function App() {
         </div>
       </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
