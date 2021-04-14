e065e11bd4c5f8b511e6b9c00d7723bf47e76ddf
bruteforce91 <48923975+bruteforce91@users.noreply.github.com>
2021-04-14 10:15:56 +0200

    feat: create banner Cookies and implements cookie in local storage (#14)
    * feat: create banner Cookies and implements the cookie in local storage
    
    * fix:rename state
    
    * fix:bugs banner
    
    * fix:conflicts
    
    * fix:conflicts in App.js and index.html
    
    * fix:add banner in app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Components/Header/Header.jsx";
 import Activities from "./Activities.jsx";
 import Homepage from "./Components/Homepage/Homepage.jsx"
-
+import {useState,useCallback} from "react"
+import BannerCookies from "./Components/BannerCookies/BannerCookies.jsx"
 
 function App() {
+  const [isBannerVisible,setIsBannerVisible]=useState(localStorage.getItem("cookieSession"));
+
+   const cookieSession=useCallback(()=> {
+        const date = JSON.stringify(new Date());
+        localStorage.setItem("cookieSession", `date:${date}`);
+        setIsBannerVisible(true)
+        return;
+    },[])
+  
+
   return (
+    <>
     <Router>
       <div className="App">
         <Header />
@@ -17,8 +29,10 @@ function App() {
             <Activities />
           </Route>
         </Switch>
+        <BannerCookies isBannerVisible={isBannerVisible} setVisibleBanner={cookieSession}/>
       </div>
     </Router>
+    </>
   );
 }
 
