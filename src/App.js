10d3c4ec30b77cd7ad8d97476edac486df8fcff9
Michelangelo <78905868+miki-miko@users.noreply.github.com>
2021-04-14 11:03:33 +0200

    feat: implemented Activity Title component (#13)
    * feat: implemented Activity Title component
    
    * fix: ActivityTitle now use Rank component and dynamic values, CSS is cleaner

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./Components/Header/Header.jsx";
-import Activities from "./Activities.jsx";
-import Homepage from "./Components/Homepage/Homepage.jsx"
-import {useState,useCallback} from "react"
-import BannerCookies from "./Components/BannerCookies/BannerCookies.jsx"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './Components/Header/Header.jsx'
+import Activities from './Activities.jsx'
+import Homepage from './Components/Homepage/Homepage.jsx'
+import { useState, useCallback } from 'react'
+import BannerCookies from './Components/BannerCookies/BannerCookies.jsx'
 
 function App() {
-  const [isBannerVisible,setIsBannerVisible]=useState(localStorage.getItem("cookieSession"));
+  const [isBannerVisible, setIsBannerVisible] = useState(
+    localStorage.getItem('cookieSession')
+  )
 
-   const cookieSession=useCallback(()=> {
-        const date = JSON.stringify(new Date());
-        localStorage.setItem("cookieSession", `date:${date}`);
-        setIsBannerVisible(true)
-        return;
-    },[])
-  
+  const cookieSession = useCallback(() => {
+    const date = JSON.stringify(new Date())
+    localStorage.setItem('cookieSession', `date:${date}`)
+    setIsBannerVisible(true)
+    return
+  }, [])
 
   return (
     <>
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route exact path="/">
-           <Homepage />
-          </Route>
-          <Route exact path="/activities/:id">
-            <Activities />
-          </Route>
-        </Switch>
-        <BannerCookies isBannerVisible={isBannerVisible} setVisibleBanner={cookieSession}/>
-      </div>
-    </Router>
+      <Router>
+        <div className='App'>
+          <Header />
+          <Switch>
+            <Route exact path='/'>
+              <Homepage />
+            </Route>
+            <Route exact path='/activities/:id'>
+              <Activities />
+            </Route>
+          </Switch>
+          <BannerCookies
+            isBannerVisible={isBannerVisible}
+            setVisibleBanner={cookieSession}
+          />
+        </div>
+      </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
