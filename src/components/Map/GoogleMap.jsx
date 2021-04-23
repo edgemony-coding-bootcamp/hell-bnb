cf9dd61d3442e528c99c2ad4ea4c9c2ad9803984
Pietro Piraino <curciopietro91@gmail.comgit config --global user.email curciopietro91@gmail.com>
2021-04-23 15:40:56 +0200

    fix: removed not needed comment

--- src/components/Map/GoogleMap.jsx
+++ src/components/Map/GoogleMap.jsx
@@ -1,38 +1,3 @@
-// import React from "react";
-// import { GoogleApiWrapper, Marker } from "google-maps-react";
-// import { GoogleMap as Map } from "./Map.elements";
-
-// function MapContainer(props) {
-//   const { google, lat, lng } = props;
-
-//   return (
-//     <Map
-//       google={google}
-//       zoom={16}
-//       initialCenter={{
-//         lat,
-//         lng,
-//       }}
-//     >
-//       <Marker
-//         position={{
-//           lat,
-//           lng,
-//         }}
-//         icon={{
-//           url: "https://img.icons8.com/color/48/000000/marker--v1.png",
-//           scaledSize: { width: 60, height: 60 },
-//         }}
-//       />
-//     </Map>
-//   );
-// }
-// export default GoogleApiWrapper({
-//   apiKey: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo",
-// })(MapContainer);
-
-// Old version
-
 import React from "react";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 import { containerStyle } from "./Map.elements";
