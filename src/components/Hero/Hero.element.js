d9a4b8697ee1922baca24c4c6fb2de93f9cacb28
bruteforce91 <corsarosalvatore91@yahoo.it>
2021-04-29 23:04:48 +0200

    fix:css single cover

--- src/components/Hero/Hero.element.js
+++ src/components/Hero/Hero.element.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const CoverHero = styled.div`
   width: 100%;
-  max-height: 480px;
+  min-height: 270px;
   background-image: url(${(props) => props.img});
   background-size: cover;
 `;
@@ -10,4 +10,5 @@ export const CoverHero = styled.div`
 export const CoverImg = styled.img`
   width: 100%;
   height: 100%;
+  border-radius: 10px;
 `;
