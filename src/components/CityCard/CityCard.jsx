ca98b764e4c51950b9fe59a67ce5a760974d89bb
deMicale <78762283+deMicale@users.noreply.github.com>
2021-04-29 12:49:16 +0200

    cityCard and api cities

---
+++ src/components/CityCard/CityCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CityCardWrapper, CityImg, CityName } from "./CityCard.elements";
+
+const CityCard = () => (
+  <CityCardWrapper>
+    <CityImg />
+    <CityName>Stromboli</CityName>
+  </CityCardWrapper>
+);
+export default CityCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Title, Wrap, SubTitle } from "./Home.elements";
-import { fetchActivities } from "../../services/api";
+import { fetchActivities, fetchCities } from "../../services/api";
 import CarouselActivities from "../../components/CarouselActivities/CarouselActivities";
+import CityCard from "../../components/CityCard/CityCard";
 
 function Home() {
   const [activities, setActivities] = useState([]);
+  const [cities, setCities] = useState([]);
 
   useEffect(() => {
     const fetch = async () => {
       try {
-        const data = await fetchActivities();
-        setActivities(data);
+        const [dataActivities, dataCities] = await Promise.all([
+          fetchActivities(),
+          fetchCities(),
+        ]);
+        // const dataActivities = await fetchActivities();
+        // const dataCities = await fetchCities();
+        setActivities(dataActivities);
+        setCities(dataCities);
       } catch (error) {
         // eslint-disable-next-line
         console.error(error);
@@ -24,6 +32,11 @@ function Home() {
       <Title>ESPERIENZE HELLBNB</Title>
       <SubTitle>Attività uniche organizzate da esperti</SubTitle>
       {activities && <CarouselActivities activities={activities} />}
+      <CityCard />
+      {
+        // eslint-disable-next-line
+        cities && console.log(cities)
+      }
     </Wrap>
   );
 }
