0598f3d3f6c8a2b66bf7c39c8fa1c1a175e75ee3
BarbaraBellassai <78905864+BarbaraBellassai@users.noreply.github.com>
2021-04-30 10:07:06 +0200

    Feat/comments (#81)
    * feat: Comments/Mobile version

--- src/components/Layout/Layout.element.js
+++ src/components/Layout/Layout.element.js
@@ -143,7 +143,7 @@ const WrapExperiences = styled.div`
 `;
 
 const WrapGeneric = styled.div`
-  /* min-height: 100px; */
+  min-height: fit-content;
   width: 100%;
   margin: 20px 0;
   background-color: ${(props) =>
